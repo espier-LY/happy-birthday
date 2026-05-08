73cd733c432ff0336e6f2dee31bafc797e7d69bb
Liu Yang <3065640477@qq.com>
2026-05-08 22:52:18 +0800

    update

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 var imageEl = document.getElementById('image');
+var container = document.getElementById('container');
 
-function handleTap() {
+// 确保第一张图立即显示
+imageEl.src = '1.jpg';
+
+// 切换到第二张图 - 设为全局函数供 HTML 调用
+function goToSecond() {
     imageEl.src = '2.png';
 }
 
-imageEl.addEventListener('click', handleTap);
-imageEl.addEventListener('touchstart', function(e) {
+// 点击切换
+container.onclick = goToSecond;
+
+// 移动端触摸切换
+container.addEventListener('touchend', function(e) {
     e.preventDefault();
-    handleTap();
-}, { passive: false });
+    goToSecond();
+});
 
 // 预加载第二张图
 var preloader = new Image();
