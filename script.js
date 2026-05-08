f16075a13ec872ebb8b80c1e048cfdd2dfb8e1e1
Liu Yang <3065640477@qq.com>
2026-05-08 23:05:31 +0800

    优化

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 var imageEl = document.getElementById('image');
-var hiddenBtn = document.getElementById('hiddenBtn');
+var hasChanged = false;
 
 function goToSecond() {
+    if (hasChanged) return;
+    hasChanged = true;
     imageEl.src = '2.png';
-    hiddenBtn.style.display = 'none';
 }
 
-hiddenBtn.onclick = goToSecond;
+// 监听整个文档的点击和触摸
+document.addEventListener('click', goToSecond);
+document.addEventListener('touchend', function(e) {
+    e.preventDefault();
+    goToSecond();
+}, { passive: false });
 
 // 预加载第二张图
 var preloader = new Image();
