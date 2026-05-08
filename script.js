8351eb1a1bb5570418590ad984b2d9946ddfeb2a
Liu Yang <3065640477@qq.com>
2026-05-08 22:54:56 +0800

    update

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-var imageEl = document.getElementById('image');
-var container = document.getElementById('container');
+var image1 = document.getElementById('image1');
+var image2 = document.getElementById('image2');
 
-// 确保第一张图立即显示
-imageEl.src = '1.jpg';
-
-// 切换到第二张图 - 设为全局函数供 HTML 调用
 function goToSecond() {
-    imageEl.src = '2.png';
+    image1.style.display = 'none';
+    image2.style.display = 'block';
 }
-
-// 点击切换
-container.onclick = goToSecond;
-
-// 移动端触摸切换
-container.addEventListener('touchend', function(e) {
-    e.preventDefault();
-    goToSecond();
-});
-
-// 预加载第二张图
-var preloader = new Image();
-preloader.src = '2.png';
